2c9100363b718a19edd9ba14c0da4246b8920eaf
bluestarburst <hargreaves.bryant@gmail.com>
2024-02-17 17:23:05 -0600

    themes

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
+
+import CssBaseline from '@mui/material/CssBaseline';
+import "./app.css"
+
+const theme = createTheme({
+	palette: {
+		mode: "dark",
+		primary: {
+			main: purple[500],
+		},
+		secondary: {
+			main: green[500],
+		},
+	},
+});
+
 function App() {
 	return (
-		<div>
-			<h1>Hello, world!</h1>
-		</div>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<div className="page">
+			<h1>Hello!</h1>
+			</div>
+		</ThemeProvider>
 	);
 }
 
