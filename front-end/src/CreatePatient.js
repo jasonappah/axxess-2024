3ba8bb61a6e18c4de6e886507ccc5d9a40a5aa14
BukunmiA <bukunmiadegbonmire@gmail.com>
2024-02-17 18:29:21 -0600

    create Patient

--- front-end/src/CreatePatient.js
+++ front-end/src/CreatePatient.js
@@ -1,9 +1,47 @@
-
+import React, { useEffect, useState } from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function CreatePatient() {
     return (
-        <div>
-            <h1>Create Patient</h1>
-        </div>
-    );
+        <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      
+      <div>
+        <TextField
+          required
+          id="filled-required"
+          label="Name"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          disabled
+          id="filled-disabled"
+          label="ID"
+          defaultValue="Hello World"
+          variant="filled"
+        />
+        <TextField
+          id="filled-password-input"
+          label="Medicine"
+          type="password"
+          autoComplete="current-password"
+          variant="filled"
+        />
+
+       <TextField id="filled-basic" label="Filled" variant="filled" />
+       
+      </div>
+     
+    </Box>
+  );
+    
 }
