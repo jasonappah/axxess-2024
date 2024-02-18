2f817411f9c1234ff0701611a3fa3e069112fb1d
bluestarburst <hargreaves.bryant@gmail.com>
2024-02-17 23:53:18 -0600

    revert

--- front-end/src/CreatePatient.js
+++ front-end/src/CreatePatient.js
@@ -2,14 +2,12 @@ import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleMinus, faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import Button from "@mui/material/Button";
 
-import "./createPatient.css";
-
 export default function CreatePatient() {
-	const [numMedicine, setnumMedicine] = useState([]);
-	const [numDosage, setnumDosage] = useState([]);
+	const [numMedicine, setnumMedicine] = useState([""]);
+	const [numDosage, setnumDosage] = useState([""]);
 
 	return (
 		<Box
@@ -20,7 +18,7 @@ export default function CreatePatient() {
 			noValidate
 			autoComplete="off"
 		>
-			<div className="create-fields">
+			<div>
 				<header>
 					<h1 align="center">Create Patient</h1>
 				</header>
@@ -34,57 +32,27 @@ export default function CreatePatient() {
 				/>
 				<TextField required id="filled-required" label="ID" variant="filled" />
 				{numMedicine.map((medicine, index) => (
-					<div className="medicine">
-						<TextField
-							required
-							key={index}
-							value={medicine}
-							id="filled-required"
-							label="Medicine"
-							variant="filled"
-							disabled
-						/>
-						<TextField
-							required
-							value={numDosage[index]}
-							id="filled-basic"
-							label="Dosage"
-							variant="filled"
-							disabled
-						/>
-						<FontAwesomeIcon
-							icon={
-								index == numMedicine.length - 1 ? faCirclePlus : faCircleMinus
-							}
-							className="icon"
-							size="2x"
-							onClick={(e) => {
-								if (index == numMedicine.length - 1) {
-									var temp = [...numMedicine];
-									temp.push("");
-									setnumMedicine(temp);
-
-									var temp = [...numDosage];
-									temp.push("");
-									setnumDosage(temp);
-								} else {
-									var temp = [...numMedicine];
-									temp.splice(index, 1);
-									setnumMedicine(temp);
-
-									var temp = [...numDosage];
-									temp.splice(index, 1);
-									setnumDosage(temp);
-								}
-							}}
-						/>
-					</div>
+					<TextField
+						required
+						key={index}
+						id="filled-required"
+						label="Medicine"
+						variant="filled"
+					/>
 				))}
 
-				{/* <FontAwesomeIcon
+				{numMedicine.map((Dosage, index) => (
+					<TextField
+						required
+						id="filled-basic"
+						label="Dosage"
+						variant="filled"
+					/>
+				))}
+
+				<FontAwesomeIcon
 					icon={faCirclePlus}
 					className="icon"
-					size="2x"
 					onClick={(e) => {
 						var temp = [...numMedicine];
 						temp.push("");
@@ -94,7 +62,7 @@ export default function CreatePatient() {
 						temp.push("");
 						setnumDosage(temp);
 					}}
-				/> */}
+				/>
 
                 <Box sx={{display:'flex',flexDirection:'column'}}>
                 {numMedicine.map((medicine,index) =>(
