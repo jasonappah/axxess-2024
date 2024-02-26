91c2ee4e20b3907d9f13f518d637854329a4a1c1
Jason Antwi-Appah <hey@jasonaa.me>
2024-02-26 01:31:52 -0600

    Implement asynchronous cron job for dispensation

--- user_api/worker/celeryconfig.py
+++ user_api/worker/celeryconfig.py
@@ -8,3 +8,10 @@ result_serializer = "json"
 accept_content = ["json"]
 timezone = "America/Chicago"
 enable_utc = True
+
+beat_schedule = {
+    "create-pill-dispenses-schedule": {
+        "task": "worker.tasks.create_scheduled_dispenses",
+        "schedule": 30.0,
+    },
+}
--- user_api/worker/tasks.py
+++ user_api/worker/tasks.py
@@ -1,8 +1,18 @@
 from celery import Celery
+from api.database import get_session
+from api.public.user.crud import read_prescriptions
+
 app = Celery('tasks')
 app.config_from_object('worker.celeryconfig')
 
 @app.task
-def add(x, y):
-    return x + y
-
+def create_scheduled_dispenses():
+    db = next(get_session())
+    prescriptions = read_prescriptions(db)
+    
+    updates = [prescription.handle_dispense() for prescription in prescriptions]
+    
+    if any(updates):
+        db.commit()
+    return updates
+    
